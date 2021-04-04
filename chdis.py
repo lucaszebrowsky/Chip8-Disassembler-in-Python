3bf08c5c5e566e7baf19f57012c907c03ea10e6c
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2021-04-04 10:02:36 +0200

    Update chdis.py

--- chdis.py
+++ chdis.py
@@ -148,7 +148,7 @@ class dissassembler:
             try:
                 code_ = next(code).hex()
                 final_code = decoder(code_)
-                output_file = open(f"output-{self.filename}.txt", "a")
+                output_file = open(f"output-{os.path.basename(self.filename)}.txt", "a")
                 
                 if final_code is not None:
                     output_file.write(f"{final_code}\n")
